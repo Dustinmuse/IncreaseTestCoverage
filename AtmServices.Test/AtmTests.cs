3a5f05f312417dcb52e512f6d18946245543d1df
Dustin Muse <122392558+Dustinmuse@users.noreply.github.com>
2025-11-12 02:18:25 +0000

    Created more tests to have 100% coverage over the code.

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -31,4 +31,46 @@ public class AtmTests
 
     }
 
+    [Fact]
+
+    public void Test_Withdraw_Failure_Insufficient_Funds()
+
+    {
+
+        var result = testAtm.withdraw(200);
+
+        Assert.False(result);
+
+        Assert.Equal(100, testAtm.getBalance());
+
+    }
+
+    [Fact]
+
+    public void Test_Deposit()
+
+    {
+
+        var result = testAtm.deposit(200);
+
+        Assert.True(result);
+
+        Assert.Equal(300, testAtm.getBalance());
+
+    }
+
+    [Fact]
+
+    public void Test_Deposit_Failure_Less_Than_Minimum_Required()
+
+    {
+
+        var result = testAtm.deposit(-100);
+
+        Assert.False(result);
+
+        Assert.Equal(100, testAtm.getBalance());
+
+    }
+
 }
